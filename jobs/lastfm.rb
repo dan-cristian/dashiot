71d15169c9142fcdcfb93c3cb8d1176d68605422
Dan Cristian <dan.cristian@gmail.com>
2016-12-31 03:30:41 +0200

    +userloved lastfm

--- jobs/lastfm.rb
+++ jobs/lastfm.rb
@@ -1,5 +1,7 @@
 require 'open-uri'
 require 'xmlsimple'
+require 'cgi'
+
 
 username = 'dancri77'
 api_key = '0589cb68cd7478b22f5854db59d00ae8'
@@ -17,6 +19,16 @@ SCHEDULER.every '45s', :first_in => 0 do |job|
 		song = XmlSimple.xml_in(response.body, { 'ForceArray' => false })['recenttracks']['track'][0]
 		song['nowplaying'] == "true" ? track_status = "last.fm playing" : track_status = "last.fm last played"
 		song['image'][1]['content'].nil? ? image = "assets/no-album-art.jpg" : image = song['image'][1]['content']
-		send_event('lastfm', { :status => 'ok', :cover => image, :artist => song['artist']['content'], :track => song['name'], :title => track_status})
+		artist = CGI.escape(song['artist']['content'])
+		track = CGI.escape(song['name'])
+		response = http.request(Net::HTTP::Get.new("/2.0/?method=track.getInfo&user=#{username}&api_key=#{api_key}&artist=#{artist}&track=#{track}"))
+		info = XmlSimple.xml_in(response.body, { 'ForceArray' => false })
+		if info['status'] != 'failed'
+			loved = info['track']['userloved']
+		else
+			loved = -1
+		end
+		send_event('lastfm', { :status => 'ok', :cover => image, :artist => song['artist']['content'], 
+			:track => song['name'], :title => track_status, :loved => loved})
 	end
 end
--- widgets/lastfm/lastfm.coffee
+++ widgets/lastfm/lastfm.coffee
@@ -9,3 +9,4 @@ class Dashing.lastfm extends Dashing.Widget
     # Handle incoming data
     # You can access the html node of this widget with `@node`
     # $(@node).fadeOut().fadeIn()
+    @set 'is-loved', data.loved
