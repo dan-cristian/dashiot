787ceea305ef2dc932394e295cfb7b597bf5bcaa
Dan Cristian <dan.cristian@gmail.com>
2017-01-06 00:04:11 +0200

    mpd improvements

--- jobs/mpd.rb
+++ jobs/mpd.rb
@@ -1,5 +1,7 @@
 require 'ruby-mpd'
 
+$mpd_mutex = Mutex.new
+
 class Cmpd
   def initialize(zone_name, port, ip)
     @zone_name = zone_name
@@ -12,68 +14,101 @@ class Cmpd
 end
 
 @IP='192.168.0.9'
-$mpd_server_name_list = ['living', 'headset', 'beci', 'dormitor', 'baie', 'pod']
-$mpd_server_port_list = [6600, 6601, 6602, 6603, 6604, 6605]
-$mpd_server_ip_list   = [@IP, @IP, @IP, @IP, @IP, @IP]
-$mpd_list             = Array.new(6)
-
 $cmpd_list = [Cmpd.new('living', 6600, @IP), Cmpd.new('headset', 6601, @IP), Cmpd.new('beci', 6602, @IP),
   Cmpd.new('dormitor', 6603, @IP), Cmpd.new('baie', 6604, @IP), Cmpd.new('pod', 6605, @IP)]
 $mpd_current_index = nil
 
 post '/mpd/change_mpd' do
-  mpd_name = params["mpd_name"]
-  for i in 0..$cmpd_list.count - 1
-    if $cmpd_list[i].zone_name == mpd_name
-      puts "Selected zone index #{i}"
-      $mpd_current_index = i
-      update_mpd()
-      return
+  $mpd_mutex.synchronize do
+    mpd_name = params["mpd_name"]
+    for i in 0..$cmpd_list.count - 1
+      if $cmpd_list[i].zone_name == mpd_name
+        puts "Selected zone index #{i}"
+        $mpd_current_index = i
+        update_mpd()
+        return
+      end
     end
+    puts "Warning, no mpd instance found for name #{mpd_name}"
   end
-  puts "Warning, no mpd instance found for name #{mpd_name}"
 end
 
 post '/mpd/exec_cmd' do
-  cmd_name = params["cmd_name"]
-  puts "Executing command #{cmd_name}"
-  mpd = $cmpd_list[$mpd_current_index].mpd
-  mpd.connect unless mpd.connected?
-  result = mpd.send(cmd_name)
-  puts"Exec result #{result}"
-  update_mpd()
-  return JSON.generate({"status" => "OK"})
-end
-
-post '/mpd/exec_cmd_cust' do
-  cmd_name = params["cmd_name"]
-  puts "Executing custom command #{cmd_name}"
-  mpd = $cmpd_list[$mpd_current_index].mpd
-  mpd.connect unless mpd.connected?
-  case cmd_name
-  when 'toggle'
-    puts "Set pause to #{mpd.playing?}"
-    mpd.pause = mpd.playing?
-  when 'volume_up'
-    mpd.volume = mpd.volume + 5
-  when 'volume_down'
-    mpd.volume = mpd.volume - 5
-  when 'repeat'
-    mpd.repeat = !mpd.repeat?
-  when 'random'
-    mpd.random = !mpd.random?
-  else
-    puts "Unknown command #{cmd_name}"
+  $mpd_mutex.synchronize do
+    cmd_name = params["cmd_name"]
+    puts "Executing command #{cmd_name}"
+    mpd = $cmpd_list[$mpd_current_index].mpd
+    mpd.connect unless mpd.connected?
+    result = mpd.send(cmd_name)
+    puts"Exec result #{result}"
+    update_mpd()
+    return JSON.generate({"status" => "OK"})
   end
-  update_mpd()
-  return JSON.generate({"status" => "OK"})
 end
 
+# http://www.rubydoc.info/github/archSeer/ruby-mpd/master/MPD
+post '/mpd/exec_cmd_cust' do
+  $mpd_mutex.synchronize do
+    cmd_name = params["cmd_name"]
+    puts "Executing custom command #{cmd_name}"
+    mpd = $cmpd_list[$mpd_current_index].mpd
+    mpd.connect unless mpd.connected?
+    case cmd_name
+    when 'toggle'
+      puts "Set pause to #{mpd.playing?}"
+      if mpd.playing? or mpd.paused?
+        mpd.pause = mpd.playing?
+      elsif mpd.stopped?
+        mpd.play
+      end
+    when 'volume_up'
+      mpd.volume = mpd.volume + 5
+    when 'volume_down'
+      mpd.volume = mpd.volume - 5
+    when 'repeat'
+      mpd.repeat = !mpd.repeat?
+    when 'random'
+      mpd.random = !mpd.random?
+    when /output:/
+      out_zone = cmd_name.split(':')[1]
+      toggle_output(mpd, out_zone)
+    else
+      puts "Unknown command #{cmd_name}"
+    end
+    update_mpd()
+    return JSON.generate({"status" => "OK"})
+  end
+end
+
+post '/mpd/exec_cmd' do
+  $mpd_mutex.synchronize do
+    cmd_name = params["cmd_name"]
+    puts "Executing command #{cmd_name}"
+    mpd = $cmpd_list[$mpd_current_index].mpd
+    mpd.connect unless mpd.connected?
+    result = mpd.send(cmd_name)
+    puts"Exec result #{result}"
+    update_mpd()
+    return JSON.generate({"status" => "OK"})
+  end
+end
+
+def toggle_output(mpd, output_name)
+  for i in 0..mpd.outputs.count - 1
+    out = mpd.outputs[i]
+    if out[:outputname].include? output_name
+      puts "Toggling output #{out[:outputname]}, before is #{out[:outputenabled]}"
+      mpd.toggleoutput(out[:outputid])
+      return
+    end
+  end
+  puts "Cannot toggle output for zone #{output_name}"
+end
 
 def init()
   for i in 0..$cmpd_list.count - 1
-	  mpd = MPD.new $cmpd_list[i].ip, $cmpd_list[i].port
-	  mpd.connect
+    mpd = MPD.new $cmpd_list[i].ip, $cmpd_list[i].port
+    mpd.connect
     puts "Connected" if mpd.connected?
     $cmpd_list[i].mpd = mpd
   end
@@ -82,33 +117,55 @@ end
 
 def update_mpd()
   init() if $cmpd_list[0].mpd.nil?
-  puts "Updating mpd #{$mpd_server_name_list[$mpd_current_index]}"
+  puts "Updating mpd #{$cmpd_list[$mpd_current_index].zone_name}"
   mpd = $cmpd_list[$mpd_current_index].mpd
+  #mpd.disconnect if mpd.connected? 
   mpd.connect unless mpd.connected?
   unless mpd.current_song.nil?
     song = mpd.current_song.artist + ' - ' + mpd.current_song.title
   else
     song = '(none)'
   end
-  if mpd.playing?
-    playstate = 'playing'
-  elsif mpd.paused?
-    playstate = 'paused'
-  elsif mpd.stopped?
-    playstate = 'stopped'
-  else playstate = 'undefined!'
+  for i in 0..2
+    begin
+      if mpd.status[:state] == :play
+        playstate = 'playing'
+      elsif mpd.status[:state] == :pause
+        playstate = 'paused'
+      elsif mpd.status[:state] == :stop
+        playstate = 'stopped'
+      else playstate = 'undefined!'
+      end
+      mpd_zone = $cmpd_list[$mpd_current_index].zone_name
+      mpd_random = mpd.random? ? 'on' : 'off'
+      mpd_repeat = mpd.repeat? ? 'on' : 'off'
+      outputs_enabled = []
+      #outputs_disabled = []
+
+      for i in 0..mpd.outputs.count - 1
+        out = mpd.outputs[i]
+        if out[:outputenabled]
+          outputs_enabled << out[:outputname]
+        #else
+        #  outputs_disabled << out[:outputname]
+        end
+      end
+      break
+    rescue => e
+      puts "!!!!!!!!!!!!!!!!!!! That crash again, err=#{e}"
+    end
   end
-  mpd_zone = $cmpd_list[$mpd_current_index].zone_name
-  mpd_random = mpd.random? ? 'on' : 'off'
-  mpd_repeat = mpd.repeat? ? 'on' : 'off'
-  puts "Updating song=#{song} state=#{playstate}"
-  send_event('mpd', mpd_song: song, mpd_volume: mpd.volume, mpd_playstate: playstate, 
-    mpd_zone: mpd_zone, mpd_random: mpd_random, mpd_repeat: mpd_repeat)
+  #puts "Updating song=#{song} state=#{playstate} zone=#{mpd_zone}"
+  send_event('mpd', mpd_song: song, mpd_volume: mpd.volume, mpd_playstate: playstate,
+    mpd_zone: mpd_zone, mpd_random: mpd_random, mpd_repeat: mpd_repeat,
+    outputs_enabled: outputs_enabled)
 end
 
 SCHEDULER.every '20s', :first_in => 0 do |job|
   run_start = Time.now
-  update_mpd()
+  $mpd_mutex.synchronize do
+    update_mpd()
+  end
   elapsed = (Time.now - run_start).to_i
   puts "MPD duration=#{elapsed} seconds"
 end
--- widgets/mpd/mpd.coffee
+++ widgets/mpd/mpd.coffee
@@ -4,38 +4,76 @@ class Dashing.mpd extends Dashing.ClickableWidget
   ready: ->
     
 
-  onData: (data) ->
-    @set 'mpd_playstate', data.mpd_playstate
-    @set 'mpd_random', data.mpd_random
-    @set 'mpd_repeat', data.mpd_repeat
-    @select_mpd("mpd:" + data.mpd_zone, null)
-    
-  select_mpd: (name, target) ->
+  select_mpd: (name, target, do_post) ->
+    console.log('select_mpd started for ' + name)
     cells = $(@node).find(".zone-name")
+    #unselect all
     for cell in cells
-      cell.style.borderColor=''
+      if cell.className != 'zone-name'
+        cell.className = 'zone-name'
       if target == null
         if cell.id == name
           target = cell
-    #unselect all
-    target.style.borderColor='red'
-    #update interface
-    $.post '/mpd/change_mpd', mpd_name: name
+    if target == null
+      console.log('!!!! Warning target is null for ' + name)
+      return
+    target.className = 'zone-name inverted'
+    if do_post
+      #update interface
+      $.post '/mpd/change_mpd', mpd_name: name
+    console.log('select_mpd completed')
   
-  exec_cmd: (cmd_name, target) ->
+  exec_cmd: (cmd_name) ->
     $.post '/mpd/exec_cmd', cmd_name: cmd_name
 
-  exec_cmd_cust: (cmd_name, target) ->
+  exec_cmd_cust: (cmd_name) ->
     $.post '/mpd/exec_cmd_cust', cmd_name: cmd_name
+
+  refresh_output: (enabled_list) ->
+    console.log('refresh_output started')
+    cells = $(@node).find(".output")
+    for cell in cells
+      if cell.className != 'output'
+        cell.className = 'output'
+      if cell.id.search("output:") >= 0
+        zone = (cell.id.split ":")[1]
+        @set 'output_'+zone, 'off'
+        for out in enabled_list
+          #fix this
+          if out.search(zone) >= 0
+            #console.log('Found cell ' + zone + ' for output ' + out)
+            cell.className='output inverted'
+            @set 'output_'+zone, 'on'
+    console.log('refresh_output completed')
+
+  update_volume: (volume) ->
+    elements = $(@node).find('.volume')
+    for element in elements
+      if element.id == 'mpd:volume'
+        element.value = volume
 
   onClick: (event) ->
     console.log("event: " + event.target.id)
     command = event.target.id.split ":"
-    
     #if /mpd:/.test(event.target.id)
     switch command[0]
-      when 'mpd' then @select_mpd(command[1], event.target)
-      when 'cmd' then @exec_cmd(command[1], event.target)
-      when 'cmd_cust' then @exec_cmd_cust(command[1], event.target)
-      when 'output' then console.log("output")
-  
+      when 'mpd' then @select_mpd(command[1], event.target, true)
+      when 'cmd' then @exec_cmd(command[1])
+      when 'cmd_cust' then @exec_cmd_cust(command[1])
+      when 'output' then @exec_cmd_cust(event.target.id)
+    console.log('onclick completed')
+
+  onData: (data) ->
+    @set 'mpd_playstate', data.mpd_playstate
+    @set 'mpd_random', data.mpd_random
+    @set 'mpd_repeat', data.mpd_repeat
+    if data.mpd_zone != null
+      @select_mpd("mpd:" + data.mpd_zone, null, false)
+    else
+      console.log('Error mpd zone is missing at onData')
+    if data.outputs_enabled != null
+      @refresh_output(data.outputs_enabled)
+    else
+      console.log('Error, enabled outputs missing')
+    @update_volume(data.mpd_volume)
+    
